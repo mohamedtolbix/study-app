971bbbc17c94a70e6adcdfb57e0ac5ab64364ce6
mohamed <mohamedtolbix01@gmail.com>
2025-12-01 10:00:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+// import Children from './children';
+import React from "react";
 
-function App() {
+const App = () => {
+  const handleClick = (e) => {
+    e.target.src = "/img/cha3el.png"; 
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table border="1" cellPadding="5" cellSpacing="0">
+      <tbody>
+        <tr>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="1"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src="/img/tafi.png"   alt="2"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src= "/img/tafi.png"
+              alt="3"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="4"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="5"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="6"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              src="/img/tafi.png "
+              alt="7"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="8"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+          <td>
+            <img
+              src="/img/tafi.png"
+              alt="9"
+              onClick={handleClick} style={{width:"60px"}}
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
-}
+};
 
 export default App;
